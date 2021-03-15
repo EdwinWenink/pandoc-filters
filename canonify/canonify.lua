30ac4e5ead4da2c18833cdcce1087d329f74ec00
EdwinWenink <edwinwenink@hotmail.com>
2021-03-15 21:17:59 +0100

    Do not prepend path for URLs

--- canonify/canonify.lua
+++ canonify/canonify.lua
@@ -5,6 +5,9 @@ function Link(el)
     el.target = string.gsub(el.target, "%.md", "")
     -- e.g. in Hugo disablePathToLower=false is default behavior
     -- this is only needed because I only use the filename in my markdown links
-    el.target = canon .. text.lower( el.target )
+    -- Do not prepend path is target is an URL
+    if not string.find(el.target, "http") then
+        el.target = canon .. text.lower( el.target )
+    end
     return el
 end
